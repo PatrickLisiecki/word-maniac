f9ff5c01089add82771b170483ba7b554a28fabf
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-01-10 00:36:36 -0500

    created and initialized next.js app

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl font-bold tracking-wide">Word Maniac</h1>
+    </main>
+  );
+}
