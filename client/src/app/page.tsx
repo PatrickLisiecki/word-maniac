a67d7955cd3de7a444fa8714e72c16d3940a3e0d
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-01-11 18:47:28 -0500

    refactored page layout and fixed start letter bug

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
 
 interface Settings {
   wordLength: string;
@@ -52,138 +52,140 @@ export default function Home() {
   }, [isRunning, seconds]);
 
   return (
-    <main className="w-full min-h-screen px-24">
-      <Navbar />
-      <h1 className="text-2xl font-bold tracking-wide">Word Maniac</h1>
-      {/* Settings Menu */}
-      <form
-        onSubmit={handleSubmit}
-        className="min-w-[800px] min-h-[300px] bg-gray-200 p-4 flex flex-col items-center"
-      >
-        {/* Word Length Slider */}
-        <div className="w-full min-h-[40px] bg-blue-300 flex flex-col justify-center p-2">
-          <label
-            htmlFor="wordLength"
-            className="block mb-2 text-md font-medium text-gray-900 dark:text-white"
+    <main className="w-full max-h-screen flex flex-col">
+      <Header />
+      <section className="h-full mx-auto flex flex-row">
+        {/* Settings Menu */}
+        <div className="min-w-[400px] min-h-[600px] my-24 py-12 border-r border-gray-400">
+          {/* Timer */}
+          <div className="w-full min-h-[60px] grid place-items-center">
+            <span className="scroll-m-20 text-2xl font-semibold tracking-tight">
+              {seconds} seconds
+            </span>
+          </div>
+
+          <form
+            onSubmit={handleSubmit}
+            className="w-full p-4 flex flex-col items-center"
           >
-            Word Length - {settings.wordLength}
-          </label>
-          <input
-            type="range"
-            min="1"
-            max="20"
-            step="1"
-            value={settings.wordLength ? settings.wordLength : 1}
-            id="wordLength"
-            onChange={(e) =>
-              setSettings({ ...settings, wordLength: e.target.value })
-            }
-            className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-          ></input>
+            {/* Word Length Slider */}
+            <div className="w-full min-h-[40px] flex flex-col p-2">
+              <label
+                htmlFor="wordLength"
+                className="block mb-2 scroll-m-20 text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+              >
+                Word Length - {settings.wordLength}
+              </label>
+              <input
+                type="range"
+                min="1"
+                max="20"
+                step="1"
+                value={settings.wordLength ? settings.wordLength : 1}
+                id="wordLength"
+                onChange={(e) =>
+                  setSettings({ ...settings, wordLength: e.target.value })
+                }
+                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+              ></input>
+            </div>
+
+            {/* Time Limit Slider */}
+            <div className="w-full min-h-[40px] flex flex-col p-2">
+              <label
+                htmlFor="timeLimit"
+                className="block mb-2 scroll-m-20 text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+              >
+                Time Limit - {settings.timeLimit}s
+              </label>
+              <input
+                type="range"
+                min="0"
+                max="120"
+                step="15"
+                value={settings.timeLimit ? settings.timeLimit : 0}
+                id="timeLimit"
+                onChange={(e) =>
+                  setSettings({ ...settings, timeLimit: e.target.value })
+                }
+                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+              ></input>
+            </div>
+
+            {/* Start Letter Input */}
+            <div className="w-full min-h-[40px] flex flex-col p-2">
+              <label
+                htmlFor="startLetter"
+                className="block mb-2 scroll-m-20 text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+              >
+                Start Letter - {settings.startLetter}
+              </label>
+              <input
+                type="text"
+                value={settings.startLetter}
+                placeholder="Starting letter"
+                id="startLetter"
+                onChange={(e) =>
+                  setSettings({ ...settings, startLetter: e.target.value })
+                }
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              ></input>
+            </div>
+
+            {/* Begin Game Button */}
+            <div className="w-full min-h-[40px] flex flex-col justify-center p-2">
+              <button
+                type="submit"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-blue-500 hover:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:hover:ring-blue-500 dark:hover:border-blue-500"
+              >
+                {isRunning ? "Stop Game" : "Begin Game"}
+              </button>
+            </div>
+          </form>
         </div>
 
-        {/* Time Limit Slider */}
-        <div className="w-full min-h-[40px] bg-blue-300 flex flex-col justify-center p-2">
-          <label
-            htmlFor="timeLimit"
-            className="block mb-2 text-md font-medium text-gray-900 dark:text-white"
+        <div className="min-w-[800px] flex flex-col my-24 py-12">
+          {/* Word Input */}
+          <form
+            onSubmit={handleWordSubmit}
+            className="p-4 flex flex-col items-center"
           >
-            Time Limit - {settings.timeLimit}s
-          </label>
-          <input
-            type="range"
-            min="0"
-            max="120"
-            step="15"
-            value={settings.timeLimit ? settings.timeLimit : 0}
-            id="timeLimit"
-            onChange={(e) =>
-              setSettings({ ...settings, timeLimit: e.target.value })
-            }
-            className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-          ></input>
-        </div>
+            {/* Start Letter Input */}
+            <div className="w-full min-h-[40px] flex flex-col justify-center p-2 relative">
+              <input
+                type="text"
+                value={currentWord}
+                placeholder="Start typing..."
+                id="currentWord"
+                onChange={(e) => setCurrentWord(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              ></input>
+              <button
+                type="submit"
+                className="absolute right-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-blue-500 hover:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:hover:ring-blue-500 dark:hover:border-blue-500"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
 
-        {/* Start Letter Input */}
-        <div className="w-full min-h-[40px] bg-blue-300 flex flex-col justify-center p-2">
-          <label
-            htmlFor="startLetter"
-            className="block mb-2 text-md font-medium text-gray-900 dark:text-white"
-          >
-            Start Letter - {settings.startLetter}
-          </label>
-          <input
-            type="text"
-            value={settings.startLetter ? settings.startLetter : "A"}
-            placeholder="A"
-            id="startLetter"
-            onChange={(e) =>
-              setSettings({ ...settings, startLetter: e.target.value })
-            }
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          ></input>
-        </div>
-
-        {/* Begin Game Button */}
-        <div className="w-full min-h-[40px] bg-blue-300 flex flex-col justify-center p-2">
-          <button
-            type="submit"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-blue-500 hover:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:hover:ring-blue-500 dark:hover:border-blue-500"
-          >
-            {isRunning ? "Stop Game" : "Begin Game"}
-          </button>
-        </div>
-      </form>
-
-      {/* Timer */}
-      <div className="min-w-[800px] min-h-[60px] bg-red-200 text-3xl tracking-wide text-center">
-        {seconds} seconds
-      </div>
-
-      {/* Word Input */}
-      <form
-        onSubmit={handleWordSubmit}
-        className="min-w-[800px] min-h-[300px] bg-gray-200 p-4 flex flex-col items-center"
-      >
-        {/* Start Letter Input */}
-        <div className="w-full min-h-[40px] bg-blue-300 flex flex-col justify-center p-2">
-          <label
-            htmlFor="startLetter"
-            className="block mb-2 text-md font-medium text-gray-900 dark:text-white"
-          >
-            Your Word
-          </label>
-          <input
-            type="text"
-            value={currentWord}
-            placeholder="Start typing..."
-            id="currentWord"
-            onChange={(e) => setCurrentWord(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          ></input>
-        </div>
-
-        {/* Submit Word */}
-        <div className="w-full min-h-[40px] bg-blue-300 flex flex-col justify-center p-2">
-          <button
-            type="submit"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:ring-blue-500 hover:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:hover:ring-blue-500 dark:hover:border-blue-500"
-          >
-            Submit Word
-          </button>
-        </div>
-      </form>
-
-      <div className="min-w-[800px] min-h-[50px] bg-gray-400 p-4 flex flex-col items-center">
-        {validWords.length > 0 &&
-          validWords.map((word: any, index: number) => {
-            return (
-              <span key={index} className="text-md text-black">
-                {word}
+          <div className="min-h-[50px] p-4 flex flex-col items-center">
+            <div className="w-full border-b border-gray-400 text-center">
+              <span className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                Word Bank
               </span>
-            );
-          })}
-      </div>
+            </div>
+            {validWords.length > 0 &&
+              validWords.map((word: any, index: number) => {
+                return (
+                  <span key={index} className="text-md text-black">
+                    {word}
+                  </span>
+                );
+              })}
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
